e15e5251c2cfb981776647a2ba0fe0d7808113fd
Valentin <valentin.rogister@gmail.com>
2025-07-07 04:09:11 +0200

    1 -> 01

--- test/test_problems/test_01.ml
+++ test/test_problems/test_01.ml
@@ -1,6 +1,6 @@
 open Ocaml_99_problems
 
 let test () =
-  assert (Problem_1.last [ 1; 2; 3 ] = Some 3);
-  assert (Problem_1.last [ 1 ] = Some 1);
-  assert (Problem_1.last [] = None)
+  assert (Problem_01.last [ 1; 2; 3 ] = Some 3);
+  assert (Problem_01.last [ 1 ] = Some 1);
+  assert (Problem_01.last [] = None)
